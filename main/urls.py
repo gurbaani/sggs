f66cec9062e0c7915de7d932223774756314165f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-29 10:48:25 -0300

    pages app included

--- main/urls.py
+++ main/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Site administration
+    path('dj-admin/', admin.site.urls),
+
+    # Local Apps
+    path('', include('pages.urls')),
+
 ]
