3aa7f5a681236d494ba1e10ff38228be715b29e2
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-29 10:48:11 -0300

    dj-database-url config imported

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+
+    # Local
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
